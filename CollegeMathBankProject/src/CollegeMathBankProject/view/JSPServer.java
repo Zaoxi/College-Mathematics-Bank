4b60147c55a4df81a8580732ed56872544a01d0c
Zaoxi <whgmlwo1000@gmail.com>
2018-05-29 22:51:31 +0900

    Project Initialization
    - Initialize JSP Project

---
+++ CollegeMathBankProject/src/CollegeMathBankProject/view/JSPServer.java
@@ -0,0 +1,39 @@
+package CollegeMathBankProject.view;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JSPServer
+ */
+@WebServlet(description = "대학 수학 문제 은행 시스템 서블릿", urlPatterns = { "/JSPServer" })
+public class JSPServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JSPServer() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
